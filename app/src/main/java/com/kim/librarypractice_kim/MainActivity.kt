fbed3c0532ae10f43230a93bd7405b14948b25ad
KJH <daeding7@naver.com>
2021-12-01 21:18:24 +0900

    권한 실제 확인 요청

--- app/src/main/java/com/kim/librarypractice_kim/MainActivity.kt
+++ app/src/main/java/com/kim/librarypractice_kim/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.kim.librarypractice_kim
 
+import android.Manifest
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.bumptech.glide.Glide.with
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.normal.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
@@ -38,7 +40,7 @@ class MainActivity : AppCompatActivity() {
                 override fun onPermissionGranted() {
 
 //                    권한 승인 -> 실제로 전화 연결 진행
-                    val myUri = Uri.parse(010-2222-3333)
+                    val myUri = Uri.parse("tel:010-2222-3333")
                     val myIntent = Intent(Intent.ACTION_CALL, myUri)
                     startActivity(myIntent)
                 }
@@ -48,10 +50,17 @@ class MainActivity : AppCompatActivity() {
 //                    권한 거절 -> 토스트로, 권한이 없어서 전화 연결 실패
                     Toast.makeText(this@MainActivity, "권한이 없어서 전화 연결이 실패했습니다.", Toast.LENGTH_SHORT).show()
                 }
-
-
             }
+
+            TedPermission.create()
+                .setPermissionListener(permissionListener)
+                .setDeniedMessage("전화 연결 권한이 필요합니다, [설정]에서 진행해 주세요")
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
+
         }
+
+
     }
 
 
